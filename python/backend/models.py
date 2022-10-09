675e3c865a3f734355bcd338d7502e14a84ca199
willidert <williderteixeira@gmail.com>
2022-10-09 01:04:05 -0400

    fix: update game model with choices

--- python/backend/models.py
+++ python/backend/models.py
@@ -1,3 +1,4 @@
+from multiselectfield import MultiSelectField
 import uuid
 from django.db import models
 
@@ -5,17 +6,26 @@ from django.db import models
 class Game(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     title = models.CharField(max_length=200)
-    question_text = models.CharField(max_length=200)
     bannerUrl = models.URLField()
 
 
 class Ad(models.Model):
+    WEEK_DAYS = (
+        (1, 'Domingo'),
+        (2, 'SEGUNDA'),
+        (3, 'TERCA'),
+        (4, 'QUARTA'),
+        (5, 'QUINTA'),
+        (6, 'SEXTA'),
+        (7, 'SABADO')
+    )
+
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=200)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     yearsPlaying = models.PositiveIntegerField()
     discord = models.CharField(max_length=200)
-    weekDays = models.CharField(max_length=200)
+    weekDays = MultiSelectField(choices=WEEK_DAYS, max_length=7)
     hourStar = models.PositiveIntegerField()
     hourEnd = models.PositiveIntegerField()
     useVoiceChannel = models.BooleanField(default=False)
