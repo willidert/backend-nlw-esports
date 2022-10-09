7305c925523eae96916eef5928750c8dfa357599
willidert <williderteixeira@gmail.com>
2022-10-09 13:59:45 -0400

    fix: add nested viewset to Ads by Game POST/GET

--- python/backend/urls.py
+++ python/backend/urls.py
@@ -1,9 +1,10 @@
-from .views import GameViewSet, AdViewSet
+from .views import GameViewSet, GameAdViewSet, AdViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
 router.register(r'games', GameViewSet, basename='game')
+router.register(r'games', GameAdViewSet, basename='game')
 router.register(r'ads', AdViewSet, basename='ads')
 
 urlpatterns = router.urls
--- python/backend/views.py
+++ python/backend/views.py
@@ -6,13 +6,26 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-class GameViewSet(viewsets.ModelViewSet):
+class GameViewSet(viewsets.ViewSet):
+    """
+    Handle game list
+    """
     queryset = Game.objects.all()
     serializer_class = GameSerializer
 
-    #se der um GET em games/{:id}/ads
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(data = serializer.data, status=status.HTTP_200_OK)
+
+class GameAdViewSet(viewsets.ViewSet):
+    """
+    get ads by game and create ad
+    """
+
+    queryset = Game.objects.all()
+
     @action(methods=['post', 'get'], detail=True, url_path='ads', url_name='ads_by_game')
-    def get_ads_by_game(self, req, pk=None):
+    def get_ads_by_game(self, req, pk=None):    
         if req.method == 'POST':
             data = req.data
             data['game'] = pk
@@ -25,21 +38,17 @@ class GameViewSet(viewsets.ModelViewSet):
             ads = Ad.objects.filter(game=pk)
             serializer = AdSerializer(ads, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    #se der um POST em games/{:id}/ads
-    # @action(methods=['post'], detail=True, url_path='ads', url_name='create_ad')
-    # def create_ad(self, req, pk=None):
-    #     data = req.data
-    #     data['game'] = pk
-    #     serializer = AdCreateSerializer(data=data)
-    #     if serializer.is_valid():
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AdViewSet(viewsets.ViewSet):
+    """
+    get discord by ad
+    """
+    queryset = Ad.objects.all()
+    serializer_class = DiscordSerializer
+
     @action(methods=['get'], detail=True, url_path='discord', url_name='get_discord_by_ad')
     def get_discord_by_ad(self, req, pk=None):
-        ad = Ad.objects.filter(id=pk).get()
-        serializer = DiscordSerializer(ad)
+        ad = self.queryset.filter(id=pk).get()
+        serializer = self.serializer_class(instance=ad)
         return Response(serializer.data, status=status.HTTP_200_OK)
